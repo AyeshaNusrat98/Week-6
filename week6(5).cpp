e2203b524226edfbd137f4447a891fd819109aa5
Ayesha Nusrat <ashkn55@gmail.com>
2025-10-27 06:58:36 +0530

    commit

--- week6(5).cpp
+++ week6(5).cpp
@@ -1,19 +1,19 @@
 /*Week-6 Task #5
 Write a c program to print Fibonacci series: 
-0,1,1,2,3,5,8,13,…………*/
+0,1,1,2,3,5,8,13,â€¦â€¦â€¦â€¦*/
 
 #include<stdio.h>
 
 main() {
 	int n, i, first=0, second=1, next;
 	
-	printf("Enter the number  of terms in Fibonacci series: \n");
+	printf("Enter the number  of terms in Fibonacci series: ");
 	scanf("%d", &n);
 	
 	printf("Fibonacci series: \n");
 	
 	for (i=1; i<=n; i++) {
-		printf("%d", first);
+		printf("%d ", first);
 		next = first + second;
 		first = second;
 		second = next;
